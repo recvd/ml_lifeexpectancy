8c3b7d421a703ab1a64c8ac37bf519c7086c1ac7
Jesse <jcahill225@gmail.com>
2019-05-15 12:13:13 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using machine learning to preduct life expectancy in census tracts of the US',
+    author='Jesse Cahill',
+    license='MIT',
+)
